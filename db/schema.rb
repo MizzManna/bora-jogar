963b05e069b737943a1172121e92aec5d33704b5
Manna Le <riverle@hotmail.com>
2023-03-09 11:09:55 -0800

    users-things

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_174821) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_185452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_174821) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone_number"
+    t.string "address"
+    t.date "birthday"
+    t.boolean "admin_user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
