6e3b5bd752460f5f58939f5d96f963373920913b
Manna Le <riverle@hotmail.com>
2023-03-09 10:27:41 -0800

    bookings-setup

--- db/schema.rb
+++ db/schema.rb
@@ -48,10 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_174821) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "bookings", "fields"
   add_foreign_key "bookings", "users"
-=======
   add_foreign_key "fields", "users"
->>>>>>> 12a6c3919faf195b61a546d111d667be1758bb09
 end
