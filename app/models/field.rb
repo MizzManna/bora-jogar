ce27dd496c73f23602f02c822ca4ef177f4973fa
joaoarthurvalle <joaoarthur_976@hotmail.com>
2023-03-13 15:56:40 -0300

    geocode3

--- app/models/field.rb
+++ app/models/field.rb
@@ -3,6 +3,8 @@ class Field < ApplicationRecord
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_address,
